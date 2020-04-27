741c7a080bec559766e2e9e1eb9a7e0a8cd0398d
Jair Domingues <jair.domingues@itsgroup.com.br>
2020-04-27 07:09:52 -0300

    ajsutes

--- src/main/java/br/com/its/latlong/rest/GeocodeController.java
+++ src/main/java/br/com/its/latlong/rest/GeocodeController.java
@@ -3,9 +3,17 @@ package br.com.its.latlong.rest;
 import java.io.IOException;
 import java.net.URLEncoder;
 
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.samples.petclinic.order.SalesOrderRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -22,11 +30,15 @@ public class GeocodeController {
 	public GeocodeResult getGeocode(@RequestParam String address) throws IOException {
 		OkHttpClient client = new OkHttpClient();
 		String encodedAddress = URLEncoder.encode(address, "UTF-8");
-		Request request = new Request.Builder().url("https://maps.googleapis.com/maps/api/geocode/json?address="
-				+ encodedAddress + "&key=AIzaSyDObGKpiIc6fl02fOytABIKpqpGxSELNxM").get().build();
 		ResponseBody responseBody = client.newCall(request).execute().body();
 		ObjectMapper objectMapper = new ObjectMapper();
 		GeocodeResult result = objectMapper.readValue(responseBody.string(), GeocodeResult.class);
 		return result;
 	}
+	
+	@PostMapping(path = "/geocodes", produces = "application/json", consumes = "application/json")
+	public ResponseEntity<?> address(@Valid @RequestBody Address address) {
+		return ResponseEntity.ok(salesOrderService.createOrder(salesOrderRequest));
+	}
+	
 }
